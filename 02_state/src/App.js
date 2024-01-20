79e0710e35758409d62b1b63b15094aed0f01f2c
Felipe Lussi <felipe-p-l@hotmail.com>
2024-01-20 10:26:50 -0300

    feat: first event handler function created

--- 02_state/src/App.js
+++ 02_state/src/App.js
@@ -6,6 +6,14 @@ const messages = [
 
 export default function App() {
   const step = 3;
+
+  function handlePrevious() {
+    return alert("Previous");
+  }
+  function handleNext() {
+    return alert("Next");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -17,10 +25,17 @@ export default function App() {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+          onMouseEnter={() => alert("mouses")}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
