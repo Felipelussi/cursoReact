aee0936a3ec593579697558783afb56d1efc1b32
Felipe Lussi <felipe-p-l@hotmail.com>
2024-01-20 13:25:59 -0300

    refactor: Uses a callback function as argument of the setStep instead of just passing the new step value

--- 02_state/src/App.js
+++ 02_state/src/App.js
@@ -10,11 +10,16 @@ export default function App() {
   const [step, setStep] = useState(1);
   const [isClose, setIsClose] = useState(true);
 
-  function handlePrevious() {
+  /* function handlePrevious() {
     if (step > 1) setStep(step - 1);
+  } */
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
   }
+
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
