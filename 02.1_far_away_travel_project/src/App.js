33c966797d37511c7468ad2bfb1ba044584bb072
Felipe Lussi <felipe-p-l@hotmail.com>
2024-01-23 09:59:52 -0300

    feat: create a sorting selector in the packing list

--- 02.1_far_away_travel_project/src/App.js
+++ 02.1_far_away_travel_project/src/App.js
@@ -80,10 +80,24 @@ function Form({ onAddItem }) {
   );
 }
 function PackingList({ items, onDelItem, onToggleItem }) {
+  const [SortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (SortBy === "input") sortedItems = items;
+  else if (SortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  else if (SortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -92,6 +106,13 @@ function PackingList({ items, onDelItem, onToggleItem }) {
           ></Item>
         ))}
       </ul>
+      <div className="actions">
+        <select value={SortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
