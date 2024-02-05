c3757edcf43e56999e1c0e6498f2159c1d6ec9f3
Felipe Lussi <felipe-p-l@hotmail.com>
2024-02-05 12:08:42 -0300

    feat: add focus to search element when loading the page or hitting enter. Do it using a REF to select a DOM element and useEffect to manipulate that element.

--- 04_use_popcorn/src/App_2.js
+++ 04_use_popcorn/src/App_2.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 import StarRating from "./StarRating.jsx";
 
@@ -170,13 +170,35 @@ function Main({ children }) {
 }
 
 function Search({ query, setQuery }) {
-  useEffect(
+  /*   useEffect(
     function () {
       const el = document.querySelector(".search");
       console.log(el);
       el.focus();
     },
     [query]
+  ); */
+
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    inputEl.current.focus();
+  }, []);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") inputEl.current.focus();
+        setQuery("");
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
   );
 
   return (
@@ -186,6 +208,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
