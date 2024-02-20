145fb7d0dcd806b0838f4ddfd9b33c25f1c25a2e
Felipe Lussi <felipe-p-l@hotmail.com>
2024-02-19 22:08:22 -0300

    Feat: create the react quiz project

--- 06_react_quiz/src/App.js
+++ 06_react_quiz/src/App.js
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter.js";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div classname="app">
+      <Header></Header>
+      <Main>
+        <p>1/15</p>
+        <p>question</p>
+      </Main>
     </div>
   );
 }
