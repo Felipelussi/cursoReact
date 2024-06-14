0793717651cc55667b431265dec10c2bdf0b2e41
Felipe Lussi <felipe-p-l@hotmail.com>
2024-06-14 13:25:38 -0300

    Fix: removed unused import

--- 04_use_popcorn/src/index.js
+++ 04_use_popcorn/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App_2";
-import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
